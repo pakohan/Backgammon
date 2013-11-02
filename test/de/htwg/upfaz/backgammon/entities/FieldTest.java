35d161ece5250ad66da6909a84b43d465d527ae0
Patrick Kohan <patrick.kohan@googlemail.com>
2013-11-02 18:00:35 +0100

    reformatted code

--- test/de/htwg/upfaz/backgammon/entities/FieldTest.java
+++ test/de/htwg/upfaz/backgammon/entities/FieldTest.java
@@ -1,31 +1,27 @@
 package de.htwg.upfaz.backgammon.entities;
 
-import de.htwg.upfaz.backgammon.entities.Field;
 import junit.framework.TestCase;
 
+public class FieldTest
+        extends TestCase {
 
-public class FieldTest extends TestCase {
+    IField field;
 
-	IField field;
+    public void setUp() {
+        field = new Field(0);
+    }
 
-	public void setUp() {
-		field = new Field(0);
-	}
+    public void testgetFieldNr() {
+        field.setFieldNr(1);
+        assertEquals(1, field.getFieldNr());
+        field.setFieldNr(0);
+        assertEquals(0, field.getFieldNr());
+    }
 
-	public void testgetFieldNr() {
-		field.setFieldNr(1);
-		assertEquals(1, field.getFieldNr());
-		field.setFieldNr(0);
-		assertEquals(0, field.getFieldNr());
-	}
-
-	public void testgetNumberStones() {
-		field.setNumberStones(1);
-		assertEquals(1, field.getNumberStones());
-		field.setNumberStones(0);
-		assertEquals(0, field.getNumberStones());
-	}
-
-
-
+    public void testgetNumberStones() {
+        field.setNumberStones(1);
+        assertEquals(1, field.getNumberStones());
+        field.setNumberStones(0);
+        assertEquals(0, field.getNumberStones());
+    }
 }
