c5228215c4093c6b23fb470fce86b367770edea8
bfcmyxa <myxa1337@hotmail.com>
2014-01-08 18:51:31 +0100

    Revert "..."
    This reverts commit e87937bbf587b93b9fe84482fd47278a96c19f51.

---
+++ src/controllers/de/htwg/upfaz/backgammon/gui/Constances.java
@@ -0,0 +1,35 @@
+package controllers.de.htwg.upfaz.backgammon.gui;
+
+public final class Constances {
+
+    private Constances() {}
+
+    public static final int DEFAULT_X_RESOLUTION = 740;
+    public static final int DEFAULT_Y_RESOLUTION = 710;
+    public static final long TIME_TO_SLEEP_IN_MS = 200;
+    public static final int FIELD_SIZE_PXL = 50;
+    public static final int LEFT_BORDER = 45;
+    public static final int LEFT_CENTER_BORDER = 295;
+    public static final int RIGHT_CENTER_BORDER = 400;
+    public static final int RIGHT_BORDER = 650;
+    public static final int UPPER_BORDER = 55;
+    public static final int UPPER_CENTER_BORDER = 280;
+    public static final int DOWN_CENTER_BORDER = 420;
+    public static final int DOWN_BORDER = 640;
+    public static final int LEFT_OUT_BORDER = 690;
+    public static final int RIGHT_OUT_BORDER = 730;
+    public static final int EATEN_LEFT_BORDER = 340;
+    public static final int FIELD_STEP = 40;
+    public static final int TOTAL_FIELDS_NR = 28;
+    public static final int DICE_X = 480;
+    public static final int DICE_Y = 295;
+    public static final int DICE_SIZE = 60;
+    public static final int FIELD_EATEN_WHITE = 25;
+    public static final int FIELD_EATEN_BLACK = 24;
+    public static final int FIELD_END_WHITE = 27;
+    public static final int FIELD_END_BLACK = 26;
+    
+    public static final int STONES_TO_WIN = 15;
+    
+	public static final String PLAYER_S_IS_THE_WINNER = "Player %s is the winner!";
+}
