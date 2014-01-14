928eedabc002673759494fef5577ff1f51491453
Patrick Kohan <patrick.kohan@googlemail.com>
2014-01-14 20:13:55 +0100

    magic numbers

--- src/controllers/de/htwg/upfaz/backgammon/controller/IGame.java
+++ src/controllers/de/htwg/upfaz/backgammon/controller/IGame.java
@@ -1,6 +1,6 @@
 package controllers.de.htwg.upfaz.backgammon.controller;
 
 public interface IGame {
-    public int MAX_JUMPS = 4;
-    public int DICE_RANDOM = 6;
+    int MAX_JUMPS = 4;
+    int DICE_RANDOM = 6;
 }
